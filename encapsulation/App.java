01b407644088f4bee4f35c8b9ad8112ae027551d
tensymb0ls <tensym8ols@gmail.com>
2023-03-15 22:21:37 +0200

    title

---
+++ encapsulation/App.java
@@ -0,0 +1,30 @@
+package encapsulation;
+public class App {
+
+    public static void main(String[] args) {
+    Student s1 = new Student();
+    s1.setName("John"); 
+    s1.setAge(18); 
+
+    Student s2 = new Student();
+    s2.setName("Jack");
+    s2.setAge(23);
+        
+    Student[] students = {s1, s2};
+    System.out.println(avgAge(students));
+    System.out.println(s2.getName());      
+    }
+
+    public static int avgAge (Student[] arr) { //средний возраст
+        int count = 0;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != null){
+                count++;
+                sum += arr[i].getAge();
+            }
+        }
+        return sum / count;
+    }
+}
+
